7245bce8fdfe187ee1e9d2546659a7f0e97cda27
Latesil <skipastas@gmail.com>
2020-07-06 12:44:12 +0300

    finish python gui template

--- src/python_template.py
+++ src/python_template.py
@@ -20,6 +20,21 @@ import os
 
 class PythonTemplate():
 
+    gpl_text = """
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
     def __init__(self, is_gui, project_id, project_name, path, is_git, license):
         self.is_gui = is_gui
         self.project_id = project_id
@@ -33,6 +48,7 @@ class PythonTemplate():
             self.create_basic_gui_structure(self.project_id, self.project_name, self.path)
             self.populate_data_folder(self.project_id, self.project_name)
             self.populate_po_dir(self.project_id, self.project_name)
+            self.populate_src_dir(self.project_id, self.project_name)
 
     def create_basic_gui_structure(self, p_id, p_name, path):
         os.makedirs(path + '/build_aux/meson')
@@ -180,13 +196,13 @@ class PythonTemplate():
         with open(self.path + '/data/' + p_id + '.gschema.xml', 'a') as file_gschema:
             file_gschema.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
             file_gschema.write("<schemalist gettext-domain=\"%s\">" % p_name)
-            file_gschema.write("\t<schema id=\"%s\" path=\"%s\">" % (p_id, p_id_reverse))
+            file_gschema.write("\t<schema id=\"%s\" path=\"/%s\">" % (p_id, p_id_reverse))
             file_gschema.write("\t</schema>\n")
             file_gschema.write("</schemalist>\n")
             file_gschema.write("\n")
 
     def populate_po_dir(self, p_id, p_name):
-        #TODO maybe there is another way to create empty file?
+        #TODO maybe there is another way to create an empty file?
         with open(self.path + '/po/LINGUAS', 'a') as file_linguas:
             file_linguas.close()
 
@@ -203,3 +219,177 @@ class PythonTemplate():
             file_potfiles.write("src/window.py\n")
             file_potfiles.write("\n")
 
+    def populate_src_dir(self, p_id, p_name):
+        class_name = "".join(w.capitalize() for w in p_name.split('-'))
+        p_name_underscore = p_name.replace('-', '_')
+        p_id_reverse = p_id.replace('.', '/')
+
+        #TODO maybe there is another way to create an empty file?
+        with open(self.path + '/src/__init__.py', 'a') as file_py_init:
+            file_py_init.close()
+
+        with open(self.path + '/src/main.py', 'a') as file_py_main:
+            file_py_main.write("# main.py\n")
+            file_py_main.write("#\n")
+            file_py_main.write("# Copyright 2020\n")
+            file_py_main.write("#\n")
+            file_py_main.write(self.gpl_text)
+            file_py_main.write("\n")
+            file_py_main.write("import sys\n")
+            file_py_main.write("import gi\n")
+            file_py_main.write("\n")
+            file_py_main.write("gi.require_version('Gtk', '3.0')\n")
+            file_py_main.write("\n")
+            file_py_main.write("from gi.repository import Gtk, Gio\n")
+            file_py_main.write("\n")
+            file_py_main.write("from .window import %sWindow\n" % class_name)
+            file_py_main.write("\n")
+            file_py_main.write("\n")
+            file_py_main.write("class Application(Gtk.Application):\n")
+            file_py_main.write("    def __init__(self):\n")
+            file_py_main.write("        super().__init__(application_id='%s',\n" % p_id)
+            file_py_main.write("                         flags=Gio.ApplicationFlags.FLAGS_NONE)\n")
+            file_py_main.write("\n")
+            file_py_main.write("    def do_activate(self):\n")
+            file_py_main.write("        win = self.props.active_window\n")
+            file_py_main.write("        if not win:\n")
+            file_py_main.write("            win = %sWindow(application=self)\n" % class_name)
+            file_py_main.write("        win.present()\n")
+            file_py_main.write("\n")
+            file_py_main.write("\n")
+            file_py_main.write("def main(version):\n")
+            file_py_main.write("    app = Application()\n")
+            file_py_main.write("    return app.run(sys.argv)\n")
+            file_py_main.write("\n")
+
+        with open(self.path + '/src/meson.build', 'a') as file_meson_build:
+            file_meson_build.write("pkgdatadir = join_paths(get_option('prefix'), get_option('datadir'), meson.project_name())\n")
+            file_meson_build.write("moduledir = join_paths(pkgdatadir, '%s')\n" % p_name_underscore)
+            file_meson_build.write("gnome = import('gnome')\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("gnome.compile_resources('%s',\n" % p_name)
+            file_meson_build.write("  '%s.gresource.xml',\n" % p_name_underscore)
+            file_meson_build.write("  gresource_bundle: true,\n")
+            file_meson_build.write("  install: true,\n")
+            file_meson_build.write("  install_dir: pkgdatadir,\n")
+            file_meson_build.write(")\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("desktop_utils = find_program('desktop-file-validate', required: false)\n")
+            file_meson_build.write("if desktop_utils.found()\n")
+            file_meson_build.write("  test('Validate desktop file', desktop_utils,\n")
+            file_meson_build.write("    args: [desktop_file]\n")
+            file_meson_build.write("  )\n")
+            file_meson_build.write("endif\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("python = import('python')\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("conf = configuration_data()\n")
+            file_meson_build.write("conf.set('PYTHON', python.find_installation('python3').path())\n")
+            file_meson_build.write("conf.set('VERSION', meson.project_version())\n")
+            file_meson_build.write("conf.set('localedir', join_paths(get_option('prefix'), get_option('localedir')))\n")
+            file_meson_build.write("conf.set('pkgdatadir', pkgdatadir)\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("configure_file(\n")
+            file_meson_build.write("  input: '%s.in',\n" % p_name)
+            file_meson_build.write("  output: '%s',\n" % p_name)
+            file_meson_build.write("  configuration: conf,\n")
+            file_meson_build.write("  install: true,\n")
+            file_meson_build.write("  install_dir: get_option('bindir')\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("%s_sources = [\n" % p_name_underscore)
+            file_meson_build.write("  '__init__.py',\n")
+            file_meson_build.write("  'main.py',\n")
+            file_meson_build.write("  'window.py',\n")
+            file_meson_build.write("]\n")
+            file_meson_build.write("\n")
+            file_meson_build.write("install_data(project_starter_sources, install_dir: moduledir)\n")
+            file_meson_build.write("\n")
+
+        with open(self.path + '/src/' + p_name + '.in', 'a') as file_exec:
+            file_exec.write("#!@PYTHON@\n")
+            file_exec.write("#\n")
+            file_exec.write("# %s.in\n" % p_name)
+            file_exec.write("#\n")
+            file_exec.write("#Copyright 2020\n")
+            file_exec.write("#\n")
+            file_exec.write(self.gpl_text)
+            file_exec.write("\n")
+            file_exec.write("import os\n")
+            file_exec.write("import sys\n")
+            file_exec.write("import signal\n")
+            file_exec.write("import gettext\n")
+            file_exec.write("\n")
+            file_exec.write("VERSION = '@VERSION@'\n")
+            file_exec.write("pkgdatadir = '@pkgdatadir@'\n")
+            file_exec.write("localedir = '@localedir@'\n")
+            file_exec.write("\n")
+            file_exec.write("sys.path.insert(1, pkgdatadir)\n")
+            file_exec.write("signal.signal(signal.SIGINT, signal.SIG_DFL)\n")
+            file_exec.write("gettext.install('%s', localedir)\n" % p_name)
+            file_exec.write("\n")
+            file_exec.write("if __name__ == '__main__':\n")
+            file_exec.write("    import gi\n")
+            file_exec.write("\n")
+            file_exec.write("    from gi.repository import Gio\n")
+            file_exec.write("    resource = Gio.Resource.load(os.path.join(pkgdatadir, '%s.gresource'))\n" % p_name)
+            file_exec.write("    resource._register()\n")
+            file_exec.write("\n")
+            file_exec.write("    from %s import main\n" % p_name_underscore)
+            file_exec.write("    sys.exit(main.main(VERSION))\n")
+
+        with open(self.path + '/src/' + p_name_underscore + '.gresource.xml', 'a') as file_gresource:
+            file_gresource.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            file_exec.write("<gresources>\n")
+            file_exec.write("  <gresource prefix=\"/%s\">\n" % p_id_reverse)
+            file_exec.write("    <file>window.ui</file>\n")
+            file_exec.write("  </gresource>\n")
+            file_exec.write("</gresources>\n")
+            file_exec.write("\n")
+
+        with open(self.path + '/src/window.py', 'a') as file_py_window:
+            file_py_window.write("# window.py\n")
+            file_py_window.write("#\n")
+            file_py_window.write("# Copyright 2020\n")
+            file_py_window.write("#\n")
+            file_py_window.write(self.gpl_text)
+            file_py_window.write("\n")
+            file_py_window.write("from gi.repository import Gtk\n")
+            file_py_window.write("\n")
+            file_py_window.write("\n")
+            file_py_window.write("@Gtk.Template(resource_path='/%s/window.ui')\n" % p_id_reverse)
+            file_py_window.write("class %sWindow(Gtk.ApplicationWindow):\n" % class_name)
+            file_py_window.write("    __gtype_name__ = '%sWindow'\n" % class_name)
+            file_py_window.write("\n")
+            file_py_window.write("    label = Gtk.Template.Child()\n")
+            file_py_window.write("\n")
+            file_py_window.write("    def __init__(self, **kwargs):\n")
+            file_py_window.write("        super().__init__(**kwargs)\n")
+            file_py_window.write("\n")
+
+        with open(self.path + '/src/window.ui', 'a') as file_window_ui:
+            file_window_ui.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+            file_window_ui.write("\n")
+            file_window_ui.write("<interface>\n")
+            file_window_ui.write("  <requires lib=\"gtk+\" version=\"3.24\"/>\n")
+            file_window_ui.write("    <template class=\"%sWindow\" parent=\"GtkApplicationWindow\">\n" % class_name)
+            file_window_ui.write("      <property name=\"default-width\">600</property>\n")
+            file_window_ui.write("    <property name=\"default-height\">300</property>\n")
+            file_window_ui.write("    <child type=\"titlebar\">\n")
+            file_window_ui.write("      <object class=\"GtkHeaderBar\" id=\"header_bar\">\n")
+            file_window_ui.write("        <property name=\"visible\">True</property>\n")
+            file_window_ui.write("        <property name=\"show-close-button\">True</property>\n")
+            file_window_ui.write("        <property name=\"title\">Hello, World!</property>\n")
+            file_window_ui.write("      </object>\n")
+            file_window_ui.write("    </child>\n")
+            file_window_ui.write("    <child>\n")
+            file_window_ui.write("      <object class=\"GtkLabel\" id=\"label\">\n")
+            file_window_ui.write("        <property name=\"label\">Hello, World!</property>\n")
+            file_window_ui.write("        <property name=\"visible\">True</property>\n")
+            file_window_ui.write("        <attributes>\n")
+            file_window_ui.write("          <attribute name=\"weight\" value=\"bold\"/>\n")
+            file_window_ui.write("          <attribute name=\"scale\" value=\"2\"/>\n")
+            file_window_ui.write("        </attributes>\n")
+            file_window_ui.write("      </object>\n")
+            file_window_ui.write("    </child>\n")
+            file_window_ui.write("    </template>\n")
+            file_window_ui.write("  </interface>\n")
