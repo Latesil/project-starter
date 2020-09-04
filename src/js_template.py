2eeca0b02d622d817112dc2639997755528d9f49
Latesil <vihilantes@gmail.com>
2020-09-04 12:38:48 +0300

    rewrite js template

--- src/js_template.py
+++ src/js_template.py
@@ -17,9 +17,9 @@
 
 import sys
 import os
-import stat
 from .project_starter_constants import constants
 from .common_files import File
+from .helpers import *
 
 class JsTemplate():
 
@@ -56,22 +56,23 @@ class JsTemplate():
         self.file.create_manifest_file(path, p_id, p_name, self.lang)
         self.file.create_meson_postinstall_file(path)
 
-        with open(path + '/' + "meson.build", 'a') as file_meson_build:
-            file_meson_build.write("project('%s',\n" % p_name)
-            file_meson_build.write("          version: '0.1.0',\n")
-            file_meson_build.write("    meson_version: '>= %s',\n" % constants['MESON_VERSION'])
-            file_meson_build.write("  default_options: [ 'warning_level=2',\n")
-            file_meson_build.write("                   ],\n")
-            file_meson_build.write(")\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("i18n = import('i18n')\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("subdir('data')\n")
-            file_meson_build.write("subdir('src')\n")
-            file_meson_build.write("subdir('po')\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("meson.add_install_script('build-aux/meson/postinstall.py')\n")
+        text = (f"project('{p_name}',\n",
+                f"          version: '0.1.0',\n",
+                f"    meson_version: '>= {constants['MESON_VERSION']}',\n",
+                f"  default_options: [ 'warning_level=2',\n",
+                f"                   ],\n",
+                f")\n",
+                f"\n",
+                f"i18n = import('i18n')\n",
+                f"\n",
+                f"\n",
+                f"subdir('data')\n",
+                f"subdir('src')\n",
+                f"subdir('po')\n",
+                f"\n",
+                f"meson.add_install_script('build-aux/meson/postinstall.py')\n",)
+
+        create_file(path + '/', 'meson.build', text)
 
     def populate_data_folder(self, p_id, p_name):
         p_id_reverse = p_id.replace('.', '/') + '/' + p_name + '/'
@@ -97,112 +98,114 @@ class JsTemplate():
         p_id_reverse_short = p_id.replace('.', '/')
         window_name = "".join(w.capitalize() for w in p_name.split('-'))
 
-        with open(self.path + '/src/main.js', 'a') as file_js_main:
-            file_js_main.write("/* main.js\n")
-            file_js_main.write(" *\n")
-            file_js_main.write(" * Copyright 2020\n")
-            file_js_main.write(" *\n")
-            file_js_main.write(self.gpl_text)
-            file_js_main.write("\n")
-            file_js_main.write("pkg.initGettext();\n")
-            file_js_main.write("pkg.initFormat();\n")
-            file_js_main.write("pkg.require({\n")
-            file_js_main.write("  'Gio': '2.0',\n")
-            file_js_main.write("  'Gtk': '3.0'\n")
-            file_js_main.write("});\n")
-            file_js_main.write("\n")
-            file_js_main.write("const { Gio, Gtk } = imports.gi;\n")
-            file_js_main.write("\n")
-            file_js_main.write("const { %sWindow } = imports.window;\n" % window_name)
-            file_js_main.write("\n")
-            file_js_main.write("function main(argv) {\n")
-            file_js_main.write("    const application = new Gtk.Application({\n")
-            file_js_main.write("        application_id: '%s',\n" % p_id)
-            file_js_main.write("        flags: Gio.ApplicationFlags.FLAGS_NONE,\n")
-            file_js_main.write("    });\n")
-            file_js_main.write("\n")
-            file_js_main.write("    application.connect('activate', app => {\n")
-            file_js_main.write("        let activeWindow = app.activeWindow;\n")
-            file_js_main.write("\n")
-            file_js_main.write("        if (!activeWindow) {\n")
-            file_js_main.write("            activeWindow = new %sWindow(app);\n" % window_name)
-            file_js_main.write("        }\n")
-            file_js_main.write("\n")
-            file_js_main.write("        activeWindow.present();\n")
-            file_js_main.write("    });\n")
-            file_js_main.write("\n")
-            file_js_main.write("    return application.run(argv);\n")
-            file_js_main.write("}\n")
+        text_main = (f"/* main.js\n",
+                     f" *\n",
+                     f" * Copyright 2020\n",
+                     f" *\n",
+                     self.gpl_text,
+                     f"\n",
+                     f"pkg.initGettext();\n",
+                     f"pkg.initFormat();\n",
+                     f"pkg.require({{\n",
+                     f"  'Gio': '2.0',\n",
+                     f"  'Gtk': '3.0'\n",
+                     f"}});\n",
+                     f"\n",
+                     f"const {{ Gio, Gtk }} = imports.gi;\n",
+                     f"\n",
+                     f"const {{ {window_name}Window }} = imports.window;\n",
+                     f"\n",
+                     f"function main(argv) {{\n",
+                     f"    const application = new Gtk.Application({{\n",
+                     f"        application_id: '{p_id}',\n",
+                     f"        flags: Gio.ApplicationFlags.FLAGS_NONE,\n",
+                     f"    }});\n",
+                     f"\n",
+                     f"    application.connect('activate', app => {{\n",
+                     f"        let activeWindow = app.activeWindow;\n",
+                     f"\n",
+                     f"        if (!activeWindow) {{\n",
+                     f"            activeWindow = new {window_name}Window(app);\n",
+                     f"        }}\n",
+                     f"\n",
+                     f"        activeWindow.present();\n",
+                     f"    }});\n",
+                     f"\n",
+                     f"    return application.run(argv);\n",
+                     f"}}\n",)
 
-        with open(self.path + '/src/meson.build', 'a') as file_meson_build:
-            file_meson_build.write("pkgdatadir = join_paths(get_option('prefix'), get_option('datadir'), meson.project_name())\n")
-            file_meson_build.write("gnome = import('gnome')\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("gnome.compile_resources('%s.src',\n" % p_id)
-            file_meson_build.write("  '%s.src.gresource.xml',\n" % p_id)
-            file_meson_build.write("  gresource_bundle: true,\n")
-            file_meson_build.write("  install: true,\n")
-            file_meson_build.write("  install_dir: pkgdatadir,\n")
-            file_meson_build.write(")\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("gnome.compile_resources('%s.data',\n" % p_id)
-            file_meson_build.write("  '%s.data.gresource.xml',\n" % p_id)
-            file_meson_build.write("  gresource_bundle: true,\n")
-            file_meson_build.write("  install: true,\n")
-            file_meson_build.write("  install_dir: pkgdatadir,\n")
-            file_meson_build.write(")\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("bin_conf = configuration_data()\n")
-            file_meson_build.write("bin_conf.set('GJS', find_program('gjs').path())\n")
-            file_meson_build.write("bin_conf.set('PACKAGE_VERSION', meson.project_version())\n")
-            file_meson_build.write("bin_conf.set('PACKAGE_NAME', meson.project_name())\n")
-            file_meson_build.write("bin_conf.set('prefix', get_option('prefix'))\n")
-            file_meson_build.write("bin_conf.set('libdir', join_paths(get_option('prefix'), get_option('libdir')))\n")
-            file_meson_build.write("bin_conf.set('datadir', join_paths(get_option('prefix'), get_option('datadir')))\n")
-            file_meson_build.write("\n")
-            file_meson_build.write("configure_file(\n")
-            file_meson_build.write("  input: '%s.in',\n" % p_id)
-            file_meson_build.write("  output: '%s',\n" % p_id)
-            file_meson_build.write("  configuration: bin_conf,\n")
-            file_meson_build.write("  install: true,\n")
-            file_meson_build.write("  install_dir: get_option('bindir')\n")
-            file_meson_build.write(")\n")
+        create_file(path + '/src/', 'main.js', text_main)
+
+        text_meson = (f"pkgdatadir = join_paths(get_option('prefix'), get_option('datadir'), meson.project_name())\n",
+                      f"gnome = import('gnome')\n",
+                      f"\n",
+                      f"gnome.compile_resources('{p_id}.src',\n",
+                      f"  '{p_id}.src.gresource.xml',\n",
+                      f"  gresource_bundle: true,\n",
+                      f"  install: true,\n",
+                      f"  install_dir: pkgdatadir,\n",
+                      f")\n",
+                      f"\n",
+                      f"gnome.compile_resources('{p_id}.data',\n",
+                      f"  '{p_id}.data.gresource.xml',\n",
+                      f"  gresource_bundle: true,\n",
+                      f"  install: true,\n",
+                      f"  install_dir: pkgdatadir,\n",
+                      f")\n",
+                      f"\n",
+                      f"bin_conf = configuration_data()\n",
+                      f"bin_conf.set('GJS', find_program('gjs').path())\n",
+                      f"bin_conf.set('PACKAGE_VERSION', meson.project_version())\n",
+                      f"bin_conf.set('PACKAGE_NAME', meson.project_name())\n",
+                      f"bin_conf.set('prefix', get_option('prefix'))\n",
+                      f"bin_conf.set('libdir', join_paths(get_option('prefix'), get_option('libdir')))\n",
+                      f"bin_conf.set('datadir', join_paths(get_option('prefix'), get_option('datadir')))\n",
+                      f"\n",
+                      f"configure_file(\n",
+                      f"  input: '{p_id}.in',\n",
+                      f"  output: '{p_id}',\n",
+                      f"  configuration: bin_conf,\n",
+                      f"  install: true,\n",
+                      f"  install_dir: get_option('bindir')\n",
+                      f")\n",)
+
+        create_file(path + '/src/', 'meson.build', text_meson)
 
         files = ['window.js', 'main.js']
         self.file.create_gresource_file(self.path, p_id, p_id_reverse, files)
 
-        with open(self.path + '/src/' + p_id + '.in', 'a') as file_in:
-            file_in.write("#!@GJS@\n")
-            file_in.write("imports.package.init({\n")
-            file_in.write("  name: \"@PACKAGE_NAME@\",\n")
-            file_in.write("  version: \"@PACKAGE_VERSION@\",\n")
-            file_in.write("  prefix: \"@prefix@\",\n")
-            file_in.write("  libdir: \"@libdir@\",\n")
-            file_in.write("  datadir: \"@datadir@\",\n")
-            file_in.write("});\n")
-            file_in.write("imports.package.run(imports.main);\n")
+        text_id_in = (f"#!@GJS@\n",
+                        f"imports.package.init({{\n",
+                        f"  name: \"@PACKAGE_NAME@\",\n",
+                        f"  version: \"@PACKAGE_VERSION@\",\n",
+                        f"  prefix: \"@prefix@\",\n",
+                        f"  libdir: \"@libdir@\",\n",
+                        f"  datadir: \"@datadir@\",\n",
+                        f"}});\n",
+                        f"imports.package.run(imports.main);\n",)
 
-        st = os.stat(self.path + '/src/' + p_id + '.in')
-        os.chmod(self.path + '/src/' + p_id + '.in', st.st_mode | stat.S_IEXEC)
+        create_file(path + '/src/', p_id + 'in', text_id_in)
+        make_executable(self.path + '/src/' + p_id + '.in')
 
-        with open(self.path + '/src/window.js', 'a') as file_js_window:
-            file_js_window.write("/* window.js\n")
-            file_js_window.write(" *\n")
-            file_js_window.write(" * Copyright 2020\n")
-            file_js_window.write(" *\n")
-            file_js_window.write(self.gpl_text)
-            file_js_window.write("\n")
-            file_js_window.write("const { GObject, Gtk } = imports.gi;\n")
-            file_js_window.write("\n")
-            file_js_window.write("var %sWindow = GObject.registerClass({\n" % window_name)
-            file_js_window.write("    GTypeName: '%sWindow',\n" % window_name)
-            file_js_window.write("    Template: 'resource:///%s/window.ui',\n" % p_id_reverse_short)
-            file_js_window.write("    InternalChildren: ['label']\n")
-            file_js_window.write("}, class %sWindow extends Gtk.ApplicationWindow {\n" % window_name)
-            file_js_window.write("    _init(application) {\n")
-            file_js_window.write("        super._init({ application });\n")
-            file_js_window.write("    }\n")
-            file_js_window.write("});\n")
-            file_js_window.write("\n")
+        text_window = (f"/* window.js\n",
+                        f" *\n",
+                        f" * Copyright 2020\n",
+                        f" *\n",
+                        self.gpl_text,
+                        f"\n",
+                        f"const {{ GObject, Gtk }} = imports.gi;\n",
+                        f"\n",
+                        f"var {window_name}Window = GObject.registerClass({{\n",
+                        f"    GTypeName: '{window_name}Window',\n",
+                        f"    Template: 'resource:///{p_id_reverse_short}/window.ui',\n",
+                        f"    InternalChildren: ['label']\n",
+                        f"}}, class %sWindow extends Gtk.ApplicationWindow {{\n",
+                        f"    _init(application) {{\n",
+                        f"        super._init({{ application }});\n",
+                        f"    }}\n",
+                        f"}});\n",
+                        f"\n")
+        
+        create_file(path + '/src/', 'window.js', text_window)
 
         self.file.create_window_ui_file(self.path, window_name)
